bb9a7329cbf9b2b93ad977c922a98c73df6d3820
David Evans <david.evans@crawco.co.uk>
2018-11-05 20:56:46 +0000

    Adding analyzer for Assert.AreEqual(expected, actual)

---
+++ src/Shouldly.Refactorings/Shouldly.Refactorings.MSTest/Shouldly.Refactorings.MSTest.Test/AssertAreEqualTests.cs
@@ -0,0 +1,88 @@
+﻿namespace Shouldly.Refactorings.MSTest.Test
+{
+    using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.CodeFixes;
+    using Microsoft.CodeAnalysis.Diagnostics;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Shouldly.Refactorings.MSTest.Analyzers;
+    using System.Text;
+    using TestHelper;
+
+    [TestClass]
+    [TestCategory("MSTest - Assert.AreEqual(expected, actual)")]
+    public class AssertAreEqualTests : CodeFixVerifier
+    {
+        [TestMethod]
+        public void NoAssertAreEqual_NoDiagnosticsRaised()
+        {
+            var testCodeBuilder = new StringBuilder();
+
+            testCodeBuilder
+                .AppendLine("namespace ConsoleApplication1")
+                .AppendLine("{")
+                .AppendLine("    using Microsoft.VisualStudio.TestTools.UnitTesting;")
+                .AppendLine("")
+                .AppendLine("    [TestClass]")
+                .AppendLine("    public class TypeName")
+                .AppendLine("    {")
+                .AppendLine("        [TestMethod]")
+                .AppendLine("        public void DoSomething()")
+                .AppendLine("        {")
+                .AppendLine("            string testValue1 = \"TEST 1\";")
+                .AppendLine("            string testValue2 = \"TEST 2\";")
+                .AppendLine("        }")
+                .AppendLine("    }")
+                .AppendLine("}");
+
+            var startCode = testCodeBuilder.ToString();
+
+            VerifyCSharpDiagnostic(startCode);
+        }
+
+        [TestMethod]
+        public void WithAssertAreEqual_DiagnosticsRaised()
+        {
+            var testCodeBuilder = new StringBuilder();
+
+            testCodeBuilder
+                .AppendLine("namespace ConsoleApplication1")
+                .AppendLine("{")
+                .AppendLine("    using Microsoft.VisualStudio.TestTools.UnitTesting;")
+                .AppendLine("")
+                .AppendLine("    [TestClass]")
+                .AppendLine("    public class TypeName")
+                .AppendLine("    {")
+                .AppendLine("        [TestMethod]")
+                .AppendLine("        public void DoSomething()")
+                .AppendLine("        {")
+                .AppendLine("            string testValue1 = \"TEST 1\";")
+                .AppendLine("            string testValue2 = \"TEST 2\";")
+                .AppendLine("            Assert.AreEqual(testValue1, testValue2);")
+                .AppendLine("        }")
+                .AppendLine("    }")
+                .AppendLine("}");
+
+            var startCode = testCodeBuilder.ToString();
+
+            var expectedDiagnostic = new DiagnosticResult
+            {
+                Id = DiagnosticIds.AssertAreEqual,
+                Message = "Assert.AreEqual(testValue1, testValue2) should be replaced with testValue1.ShouldBeNull(testValue2)",
+                Severity = DiagnosticSeverity.Info,
+                Locations = new[] { new DiagnosticResultLocation("Test0.cs", 13, 13) }
+            };
+
+            VerifyCSharpDiagnostic(startCode, expectedDiagnostic);
+        }
+
+        protected override CodeFixProvider GetCSharpCodeFixProvider()
+        {
+            return base.GetCSharpCodeFixProvider();
+        }
+
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
+        {
+            return new AssertAreEqualAnalyzer();
+        }
+    }
+}
---
+++ src/Shouldly.Refactorings/Shouldly.Refactorings.MSTest/Shouldly.Refactorings.MSTest/Analyzers/AssertAreEqualAnalyzer.cs
@@ -0,0 +1,77 @@
+﻿namespace Shouldly.Refactorings.MSTest.Analyzers
+{
+    using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.CSharp.Syntax;
+    using Microsoft.CodeAnalysis.Diagnostics;
+    using System.Collections.Immutable;
+    using System.Linq;
+
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class AssertAreEqualAnalyzer : BaseDiagnosticAnalyzer
+    {
+        /// <summary>
+        /// Returns a set of descriptors for the diagnostics that this analyzer is capable of producing.
+        /// </summary>
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(AssertAreEqualRule); } }
+
+        /// <summary>
+        /// Gets the assert is null rule.
+        /// </summary>
+        /// <value>
+        /// The assert is null rule.
+        /// </value>
+        internal static DiagnosticDescriptor AssertAreEqualRule => BuildAssertAreEqualRule();
+
+        protected override void AnalyzeInvocation()
+        {
+            var invocationExpression = Context.Node as InvocationExpressionSyntax;
+
+            var memberAccessExpression = invocationExpression.Expression as MemberAccessExpressionSyntax;
+
+            if (memberAccessExpression is null)
+            {
+                return;
+            }
+
+            var identifierNameExpression = memberAccessExpression.Expression as IdentifierNameSyntax;
+
+            if (identifierNameExpression?.Identifier.Text != "Assert")
+            {
+                return;
+            }
+
+            if (memberAccessExpression.Name.Identifier.Text != "AreEqual")
+            {
+                return;
+            }
+
+            var argumentList = invocationExpression.ArgumentList;
+
+            if (!argumentList.Arguments.Any())
+            {
+                return;
+            }
+
+            var argumentsArray = argumentList.Arguments.ToArray();
+            var expectedArgument = argumentsArray[0];
+            var actualArgument = argumentsArray[1];
+
+            RaiseDiagnostic(
+                AssertAreEqualRule, 
+                invocationExpression.GetLocation(), 
+                expectedArgument.ToString(), 
+                actualArgument.ToString());
+        }
+
+        private static DiagnosticDescriptor BuildAssertAreEqualRule()
+        {
+            return BuildRule(
+               DiagnosticIds.AssertAreEqual,
+               nameof(Resources.AssertAreEqualTitle),
+               nameof(Resources.AssertAreEqualMessageFormat),
+               nameof(Resources.AssertAreEqualCategory),
+               nameof(Resources.AssertAreEqualDescription),
+               DiagnosticSeverity.Info);
+        }
+    }
+}
